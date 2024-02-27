cb5fbf097e60e1f80ef94e485df995977896d555
Paul Richard <paulrpm@gmail.com>
2024-02-26 23:12:15 -0300

    feat: changed createtaskform class to handle task creation

--- src/components/CreateTaskForm.jsx
+++ src/components/CreateTaskForm.jsx
@@ -1,31 +1,40 @@
 import { React, useState } from "react";
 import { Alert, Button, MenuItem, Snackbar, TextField } from "@mui/material";
+import { useNavigate } from "react-router";
 
 import ErrorMessages from "../enums/ErrorMessages";
 
 function CreateTaskForm()
 {
+    const navigate = useNavigate();
+
     const [notificationType, setNotificationType] = useState("");
     const [notificationMsg, setNotificationMsg] = useState("");
     const [showNotification, setShowNotification] = useState(false);
     const [buttonDisabled, setButtonDisabled] = useState(false);
 
-    const [name, setName] = useState("");
-    const [price, setPrice] = useState("");
-    const [type, setType] = useState("");
-    const [weight, setWeight] = useState("");
+    const [title, setTitle] = useState(null);
+    const [description, setDescription] = useState(null);
+    const [type, setType] = useState(null);
 
     const host = process.env.REACT_APP_SERVER_HOST;
     const token = sessionStorage.getItem("token");
 
-    const handleSubmit = async (args) => {
-        await fetch(host + '/api/catalog/add?token=' + token, { method: 'POST', body: JSON.stringify(args) })
+    const handleSubmit = async () => {
+        await fetch(host + '/api/task/create',
+        {
+            headers: {
+                accept: 'application/json',
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + token
+            }, 
+            method: 'POST', 
+            body: buildRequestObject()
+        })
         .then((response) => response.json())
         .then((data) => {
-            if (data.success) {
-                setNotificationType('success')
-                setShowNotification(true)
-                setNotificationMsg("Produto cadastrado com sucesso!");
+            if (data.success) {                
+                navigate("/task/view/" + data.data.id);
             } else {
                 var message = ErrorMessages.DEFAULT_ERROR_MSG;
 
@@ -49,52 +58,63 @@ function CreateTaskForm()
         });
     };
 
+    const buildRequestObject = () =>
+    {
+        var requestObj = {};
+
+        if (title !== null) {
+            requestObj.title = title;
+        }
+
+        if (description !== null) {
+            requestObj.description = description;
+        }
+
+        if (type !== null) {
+            requestObj.type = type;
+        }
+
+        return JSON.stringify(requestObj);
+    }
+
     return(
         <>
             <form onSubmit={
                 (event) => {
                     event.preventDefault();
                     setButtonDisabled(true);
-                    handleSubmit({ name: name, price: price, type: type, weight: weight });
+                    handleSubmit({ title: title, description: description, type: type });
                 }
             }
             style={{width: '30%', margin: '0 auto', paddingTop: '10em'}}>
-                <TextField id="name" label="Nome" margin="dense" size="small" fullWidth
+                <TextField id="title" label="Title" margin="dense" size="small" fullWidth
                 onChange={
                     (event) => {
-                        setName(event.target.value);
+                        setTitle(event.target.value);
                     }
                 }/>
                 
-                <TextField id="type" label="Tipo" margin="dense" size="small" fullWidth
+                <TextField id="type" label="Type" margin="dense" size="small" fullWidth
                 onChange={
                     (event) => {
                         setType(event.target.value);
                     }
                 } select
                 >
-                    <MenuItem value={"Tinto"}>Tinto</MenuItem>
-                    <MenuItem value={"Branco"}>Branco</MenuItem>
-                    <MenuItem value={"Rosé"}>Rosé</MenuItem>
-                    <MenuItem value={"Licoroso"}>Licoroso</MenuItem>
-                    <MenuItem value={"Espumante"}>Espumante</MenuItem>
+                    <MenuItem value={"feature"} selected={true}>Feature</MenuItem>
+                    <MenuItem value={"bugfix"}>Bugfix</MenuItem>
+                    <MenuItem value={"hotfix"}>Hotfix</MenuItem>
                 </TextField>
 
-                <TextField id="weight" label="Peso" margin="dense" size="small" fullWidth
+                <TextField id="description" label="Description" multiline rows={10} fullWidth
                 onChange={
                     (event) => {
-                        setWeight(event.target.value);
+                        setDescription(event.target.value);
                     }
                 }
                 />
-                <TextField id="price" label="Preço" margin="dense" size="small" fullWidth
-                onChange={
-                    (event) => {
-                        setPrice(event.target.value);
-                    }
-                }
-                />
-                <Button type="submit" variant="contained" disabled={buttonDisabled}>Cadastrar</Button>
+                
+                <Button type="submit" variant="contained" disabled={buttonDisabled}>Create</Button>
             </form>
             
             <Snackbar
