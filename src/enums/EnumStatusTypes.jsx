e2bb0d5853b8069ef2d377594d1069f3a8394ff3
Paul Richard <paulrpm@gmail.com>
2024-08-28 08:53:10 -0300

    feat: created enum for general use

---
+++ src/enums/EnumStatusTypes.jsx
@@ -0,0 +1,14 @@
+const EnumStatusTypes = {
+    closed: "Closed",
+    open: "Open",
+    in_qa: "In QA",
+    in_dev: "In Dev",
+    blocked: "Blocked",
+    feature: "Feature",
+    bugfix: "Bugfix",
+    hotfix: "Hotfix",
+    removed_assignee: "removed",
+    added_assignee: "added"
+}
+
+export default EnumStatusTypes;
